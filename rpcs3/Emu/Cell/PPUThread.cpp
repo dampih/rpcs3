eef412c2ebed9c17413c05c3af999d2759716e89
eladash <elad3356p@gmail.com>
2018-12-24 16:26:05 +0200

    Get rid of RDTSC in non-tsx path

--- rpcs3/Emu/Cell/PPUThread.cpp
+++ rpcs3/Emu/Cell/PPUThread.cpp
@@ -1099,7 +1099,7 @@ extern bool ppu_stwcx(ppu_thread& ppu, u32 addr, u32 reg_value)
 
 	if (result)
 	{
-		vm::reservation_update(addr, sizeof(u32));
+		res++;
 		vm::reservation_notifier(addr, sizeof(u32)).notify_all();
 	}
 	else
@@ -1193,7 +1193,7 @@ extern bool ppu_stdcx(ppu_thread& ppu, u32 addr, u64 reg_value)
 
 	if (result)
 	{
-		vm::reservation_update(addr, sizeof(u64));
+		res++;
 		vm::reservation_notifier(addr, sizeof(u64)).notify_all();
 	}
 	else
